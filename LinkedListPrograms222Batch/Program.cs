e16233b7404ba362cd26ccff09670c761d491f82
AnilBamne <anilbamne20@gmail.com>
2022-11-28 17:46:20 +0530

    Adding Welcome Message

---
+++ LinkedListPrograms222Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListPrograms222Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To LinkedList Programs");
+        }
+    }
+}
