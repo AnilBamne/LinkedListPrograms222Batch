4a870e1200cfcec459fd4f18afde6ffd6d7dbde2
AnilBamne <anilbamne20@gmail.com>
2022-11-29 12:36:46 +0530

    Adding UC1SimpleLinkedListOf56,30and70 code

---
+++ LinkedListPrograms222Batch/CustomisedLinkedlist.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPrograms222Batch
+{
+    internal class CustomisedLinkedlist
+    {
+        public Node head;
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+        }
+        public void Display()
+        {
+            Node temp=this.head;
+            if(temp != null)
+            {
+                Console.WriteLine("Elements in linkedlist are");
+                while(temp != null)
+                {
+                    Console.Write(temp.data+" ");
+                    temp = temp.next;
+                }
+            }
+            else
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+        }
+    }
+}
--- LinkedListPrograms222Batch/Program.cs
+++ LinkedListPrograms222Batch/Program.cs
@@ -6,7 +6,23 @@ namespace LinkedListPrograms222Batch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To LinkedList Programs");
+            Console.WriteLine("Welcome To LinkedList Progra");
+            Console.WriteLine("Please choose the option\n1:UC1 create a simple linked list\n2:UC2 add 30 and 56 to 70\n");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CustomisedLinkedlist list =new CustomisedLinkedlist();
+                    list.AddLast(56);
+                    list.AddLast(30);
+                    list.AddLast(70);
+                    list.Display();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
         }
     }
 }
